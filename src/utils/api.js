48e21458c9012e6b71fddb9083ded6e61ec3443c
Nida Shaikh <aleemnida57@gmail.com>
2026-05-22 16:31:52 +0500

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://skincare-backend-alpha.vercel.app';
 
 const api = async (endpoint, options = {}) => {
     const token = localStorage.getItem('token');
